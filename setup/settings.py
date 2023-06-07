61446b75cb5781bebf6b955f1b3a0522cd83ec72
Afonso-Carvalho <afonsox57@gmail.com>
2023-06-07 19:21:21 -0300

    integrando banco de dados e cornvertendo informações em json

--- setup/settings.py
+++ setup/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path, os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yy34rr2w9-i62v$gki@q807u$s^_-a3hf8+hux6rt9-+$^wrkd'
+SECRET_KEY = SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
